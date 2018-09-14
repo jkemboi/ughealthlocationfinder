3029d34dc39bc321a928c6f2c0676d0c3d11b434
jkemboi <kemboi_john@yahoo.com>
2018-09-14 17:17:33 +0300

    Update fusiontable_settings.js

--- www/fusiontable_settings.js
+++ www/fusiontable_settings.js
@@ -243,43 +243,9 @@ $.extend(MapsLib, {
     // Bounds and center that your map defaults to when location services are off.
     // If useDefaultMapBounds is true (see section 2), this also determines which addresses get priority with autocomplete
     defaultMapBounds: {
-	// Initialize the Google Maps API v3
-var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 15,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
-});
-
-var marker = null;
-
-function autoUpdate() {
-  navigator.geolocation.getCurrentPosition(function(position) {  
-    var newPoint = new google.maps.LatLng(position.coords.latitude, 
-                                          position.coords.longitude);
-
-    if (marker) {
-      // Marker already created - Move it
-      marker.setPosition(newPoint);
-    }
-    else {
-      // Marker does not exist - Create it
-      marker = new google.maps.Marker({
-        position: newPoint,
-        map: map
-      });
-    }
-
-    // Center the map on the new position
-    map.setCenter(newPoint);
-  }); 
-
-  // Call the autoUpdate() function every 5 seconds
-  setTimeout(autoUpdate, 5000);
-}
-
-autoUpdate();
-
+	
         // Use [latitude, longitude] or address
-        center: "autoUpdate();",
+        center: "0.511150, 35.282852",
 
         // "X miles" or "X meters"
         radius: "5 miles"
